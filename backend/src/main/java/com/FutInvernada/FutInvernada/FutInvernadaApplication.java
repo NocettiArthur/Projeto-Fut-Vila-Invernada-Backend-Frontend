735d17db0e7b3889b1957cc85e961877dd701603
Arthur Nocetti <110428956+NocettiArthur@users.noreply.github.com>
2022-09-07 23:51:18 -0300

    Add files via upload

---
+++ backend/src/main/java/com/FutInvernada/FutInvernada/FutInvernadaApplication.java
@@ -0,0 +1,13 @@
+package com.FutInvernada.FutInvernada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutInvernadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutInvernadaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/FutInvernada/FutInvernada/FutInvernadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FutInvernada.FutInvernada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutInvernadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
