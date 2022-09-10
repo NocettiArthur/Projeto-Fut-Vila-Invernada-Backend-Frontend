5f6121021365a0e811cf434d77f20b5f017b1950
Arthur Nocetti <110428956+NocettiArthur@users.noreply.github.com>
2022-09-09 22:46:55 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import Header from "./Componentes/Header/Index"
+import Notificar from "./Componentes/Notificar/Index"
+import RelaçãoArtilheiro from "./Componentes/RelaçãoArtilheiro/Index"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <RelaçãoArtilheiro/>
+      <Notificar/>
+    </>
+  )
+}
+
+export default App
